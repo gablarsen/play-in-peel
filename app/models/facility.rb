5684ee708ac08bbc717e54dc2306180955c80311
Gabriel Larsen <gab.larsen@outlook.com>
2015-02-09 06:16:47 -0800

    Initial Commit

---
+++ app/models/facility.rb
@@ -0,0 +1,15 @@
+class Facility
+  include Mongoid::Document
+
+  field :Name
+  field :Aliases
+  field :Address
+  field :City
+  field :Phone
+  field :Lat
+  field :Lon
+
+  validates_uniqueness_of :Lat, :scope => :Lon
+
+  # 177
+end
